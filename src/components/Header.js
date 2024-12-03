da3841dfd9247a4df7691284d235cc3ff110b72c
InsightLaboratory <juannnmanuelll@gmail.com>
2024-12-03 03:18:50 -0300

    Mensaje descriptivo de los cambios

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from "react";
+import "./Header.css"; // Asegúrate de tener el archivo CSS
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>InsightLab Web</h1>
+    </header>
+  );
+};
+
+export default Header;
